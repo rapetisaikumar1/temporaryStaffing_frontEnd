703bf0d55232ddf5b0fe1ea014fb1a995a52cf40
Rapeti Sai Kumar <sai@Rapetis-MacBook.local>
2026-05-26 22:08:18 +0530

    feat: scroll to top on page reload

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { ScrollToTop } from "@/components/scroll-to-top";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="min-h-full flex flex-col antialiased overflow-x-hidden">
+        <ScrollToTop />
         <Navbar />
         <main className="flex-1">
           {children}
